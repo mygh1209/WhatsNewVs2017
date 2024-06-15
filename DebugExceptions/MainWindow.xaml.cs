f567c35337cfa839f4b1aeb4ad4b1013f44eaa02
guodong <test@pp.com>
2024-06-15 19:12:37 +0800

    代码格式化

--- DebugExceptions/MainWindow.xaml.cs
+++ DebugExceptions/MainWindow.xaml.cs
@@ -13,45 +13,52 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace DebugExceptions {
-  /// <summary>
-  /// Interaction logic for MainWindow.xaml
-  /// </summary>
-  public partial class MainWindow : Window {
-    public MainWindow() {
-      InitializeComponent();
+namespace DebugExceptions
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void IndexExceptionButton_Click(object sender, RoutedEventArgs e)
+        {
+            var books = new List<string>();
+            books.Add("HLSL and Pixel Shaders for XAML Developers");
+
+            string bookName = books[5]; // try to access an non-existent  item 
+                                        // comment:  a long comment that could get obscured by the exception helper.  a long comment that could get obscured by the exception helper. 
+                                        //  a long comment that could get obscured by the exception helper. a long comment that could get obscured by the exception helper. 
+        }
+
+        private void handlerButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            resultTextBlock.Text = Models.ArtDataSource.GetArtPrice("Portaments");
+
+        }
+
+        private void DivideButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultTextBlock.Text = GetAverageCustomerSales(sales: 1500, customerCount: 0).ToString();
+        }
+        private double GetAverageCustomerSales(int sales, int customerCount)
+        {
+            try
+            {
+
+                var result = sales / customerCount;
+                return result;
+            }
+            catch (DivideByZeroException ex)
+            {
+
+                throw new ArgumentOutOfRangeException("Customer Count cannot be zero", ex);
+            }
+        }
     }
-
-    private void IndexExceptionButton_Click(object sender, RoutedEventArgs e) {
-      var books = new List<string>();
-      books.Add("HLSL and Pixel Shaders for XAML Developers");
-
-      string bookName = books[5]; // try to access an non-existent  item
-                                  // comment:  a long comment that could get obscured by the exception helper.  a long comment that could get obscured by the exception helper. 
-                                  //  a long comment that could get obscured by the exception helper. a long comment that could get obscured by the exception helper. 
-    }
-
-    private void handlerButton_Click(object sender, RoutedEventArgs e) {
-
-      resultTextBlock.Text = Models.ArtDataSource.GetArtPrice("Portaments");
-
-    }
-
-    private void DivideButton_Click(object sender, RoutedEventArgs e) {
-      resultTextBlock.Text = GetAverageCustomerSales(sales: 1500, customerCount: 0).ToString();
-    }
-    private double GetAverageCustomerSales(int sales, int customerCount) {
-      try
-      {
-
-        var result = sales / customerCount;
-        return result;
-      }
-      catch (DivideByZeroException ex)
-      {
-
-        throw new ArgumentOutOfRangeException("Customer Count cannot be zero", ex);
-      }
-    }
-  }
 }
